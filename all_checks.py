c09dc65ffa1058ad4c3ea93ab247db60803dcfcf
Laura LM <100422913+Hellaynea@users.noreply.github.com>
2022-12-17 15:00:25 +0100

    Update all_checks.py
    Espaciado en  "if percent_free < min_percent or gigabytes_free < min_gb:"

--- all_checks.py
+++ all_checks.py
@@ -14,7 +14,7 @@ def check_disk_full(disk, min_gb, min_percent):
     percent_free = 100 * du.free / du.total
     #Calculate how many free gigabytes
     gigabytes_free= du.free/2**30
-    if percent_free < min_percent or gigabytes_free<min_gb:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return False
     return True
 
