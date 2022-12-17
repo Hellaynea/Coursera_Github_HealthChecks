3a1226bb1d8c37d9f70358838e0f1c07c8ecab63
Hellaynea <lauluqmar@gmail.com>
2022-12-17 14:56:38 +0100

    Se ha cambiado el concepto min_absolute por min_gb para clarificar. Además se añadió parte del codigo faltante (if check_disk_ful...)

--- all_checks.py
+++ all_checks.py
@@ -7,20 +7,23 @@ def check_reboot():
     """Return True if the computer has a pending reboot."""
     return os.path.exists("/run/ reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     #Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     #Calculate how many free gigabytes
     gigabytes_free= du.free/2**30
-    if percent_free < min_percent or gigabytes_free<min_absolute:
+    if percent_free < min_percent or gigabytes_free<min_gb:
         return False
     return True
 
 def main():
     if check_reboot():
         print("Pending Reboot.")
+        sys.exit(1)
+    if check_disk_full(disk="/", min_gb= 2, min_percent= 10):
+        print("Disk Full.")
         sys.exit(1)
     print("Everythin OK.")
     sys.exit(0)
