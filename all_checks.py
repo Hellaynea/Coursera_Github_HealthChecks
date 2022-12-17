cfce647a945ef6c72c53e38524ea9db01d856b75
Hellaynea <lauluqmar@gmail.com>
2022-12-17 15:51:12 +0100

    Allow printing more than one error message

--- all_checks.py
+++ all_checks.py
@@ -28,12 +28,17 @@ def main():
     (check_reboot, "Pending Reboot"),
     (check_root_full, "Root partition full")
     ]
+    everything_ok= True
     for check, msg in checks:
         if check():
             print(msg)
-            sys.exit(1)
+            everything_ok= False
+
     
-    print("Everythin OK.")
+    if not everything_ok:
+        sys.exit(1)
+
+    print("Everything OK.")
     sys.exit(0)
 
 
